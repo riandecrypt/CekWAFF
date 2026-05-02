da1e714f488a57281d941d44ce0fb0d2d38238e5
riandecrypt <rextenebrarum.v1@gmail.com>
2026-05-02 23:08:26 +0700

    Update 403-bypass.sh

--- 403-bypass.sh
+++ 403-bypass.sh
@@ -26,7 +26,7 @@ function banner(){
 	echo "💀💀💀💀💀💀💀💀💀"
 	echo -e "💀$green Have a beer🍺💀 $end"
 	echo "💀💀💀💀💀💀💀💀💀"
-	echo -e "     ${cyan}- twitter.com/Dheerajmadhukar : @me_dheeraj${end}"
+	echo -e "     ${cyan}- github.com/riandecrypt : ${end}"
 }
 function usage(){
         printf "Usage:\n"
